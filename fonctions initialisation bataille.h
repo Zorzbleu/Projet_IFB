f29649d61f6b07fa4cd90c6cfde8ded0bcdd105e
Zorzbleu <pierrot894@gmail.com>
2021-05-17 12:14:34 +0200

    Merge pull request #1 from Zorzbleu/Test_Github
    Passage en modulaire, code + claire (notamment nom boat dans fonctions initialisation bataille.c)
    Maintenant, plus besoin check out of bound car coordonnées générées in bound (voir generation_bateau)

--- fonctions initialisation bataille.h
+++ fonctions initialisation bataille.h
@@ -6,18 +6,33 @@
 #define PROJET_IFB_FONCTIONS_INITIALISATION_BATAILLE_H
 #define N 10
 
+typedef struct {
+  char grid[10][10];
+  int largeur;
+  int hauteur;
+} tableau ;
 typedef struct{
     int orientation;
     int length;
     int CooX;
     int CooY;
+    char id;
+    char id_dead;
+    int pv;
 } bateau ;
 
-void initialization_grille(char grid[N][N]);
-void show_grid(char grid[N][N]);
-void implentation_bateau(bateau test, char grid[N][N]);
-int verification_emplacement_bateau(bateau test, char grid[N][N]);
-void generation_bateau(bateau SizeX, char grid[N][N]);
-
-
+void initialization_grille(tableau * grid);
+void show_grid(tableau * grid);
+void implentation_bateau(bateau * boat, tableau * grid);
+int verification_emplacement_bateau(bateau * boat, tableau * grid);
+void generation_bateau(bateau * boat, tableau *grid);
+void generation_flotte(bateau * liste[], tableau * grid, int nb_bateaux);
+void debug(bateau * boat);
+void fire_artillery(int X, int Y, tableau  * boat_grid, tableau * user_grid, bateau liste[]);
+void fire_bomb(int X, int Y, tableau  * boat_grid, tableau * user_grid, bateau liste[]);
+void fire_tactical(int X, int Y, tableau  * boat_grid, tableau * user_grid, bateau liste[]);
+void fire_missile(int X, int Y, tableau *grid, tableau *user_grid, bateau  liste[]);
+int hitscan(int X, int Y, tableau *grid, tableau *user_grid, bateau  liste[]);
+int is_alive();
+int win(bateau liste[], int nb_bateaux);
 #endif //PROJET_IFB_FONCTIONS_INITIALISATION_BATAILLE_H
--- main.c
+++ main.c
@@ -1,35 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fonctions initialisation bataille.h"
-#define N 10
-
 
 
 
 int main() {
-    bateau Size2 = {0,2,0,0};
-    bateau Size31 = {0,3,0,0};
-    bateau Size4 = {0,4,0,0};
-    bateau Size32 = {0,3,0,0};
-    bateau Size5 = {0,5,0,0};
-    char grid[N][N];
+    int i, nb_bateaux = 5, shootX, shootY, type_missile, hauteur, largeur;
+    char shoot[3];
+    tableau boat_grid, user_grid;
+    boat_grid.hauteur = 10;
+    boat_grid.largeur = 10;
+    user_grid.hauteur = 10;
+    user_grid.largeur = 10;
+    bateau *liste_bateaux = NULL;
+    liste_bateaux = malloc(sizeof(bateau) * nb_bateaux);
+    if(liste_bateaux == NULL)
+        exit(1);
+    for(i = 0; i < nb_bateaux; i++) {
+        liste_bateaux[i].length = i+1;
+        liste_bateaux[i].id = 'a' + i;
+        liste_bateaux[i].id_dead = 'A' + i;
+        liste_bateaux[i].pv = liste_bateaux[i].length;
+    }
+    liste_bateaux[0].length = 1+nb_bateaux/2 ; liste_bateaux[0].pv = liste_bateaux[0].length;
+    initialization_grille(&user_grid);
+    initialization_grille(&boat_grid);
+    //generation_flotte(&liste_bateaux,boat_grid,nb_bateaux);
+    for(i = 0; i < nb_bateaux; i++) {
+       generation_bateau(&liste_bateaux[i], &boat_grid);
+    }
 
-    initialization_grille(grid);
+    do {
+        show_grid(&user_grid);
+        show_grid(&boat_grid);
+        for (i = 0; i < nb_bateaux; i++) {
+            if (liste_bateaux[i].pv == 0)
+            {printf("Le bateau %c touche le fond !\n", liste_bateaux[i].id_dead);}
+            else
+            {printf("Le bateau %c est de longueur %d et a %d points de vie.\n", liste_bateaux[i].id_dead,liste_bateaux[i].length, liste_bateaux[i].pv);}
+        }
+        do{
+            printf("Quel missile voulez-vous tirer ?\n1 - Missile normal : x restant\n2 - Missile tactique : x restant\n3 - Bombe : x restant\n4 - Missile d'artillerie : x restant");
+            scanf("%d", &type_missile);
+        } while( type_missile < 1 || type_missile > 3);
+        do {
+            printf("\nOu voulez-vous tirer ? Exemple : C7\n");
 
-    //boats_generation(grid, Size2, Size3a, Size3b,  Size4, Size5);
-    printf("Boat2\n");
-    generation_bateau(Size2, grid);
-    printf("Boat3\n");
-    generation_bateau(Size31, grid);
-    printf("Boat3b\n");
-    generation_bateau(Size32, grid);
-    printf("Boat4\n");
-    generation_bateau(Size4, grid);
-    printf("Boat5\n");
-    generation_bateau(Size5, grid);
+            gets(shoot);
 
 
-    show_grid(grid);
 
+
+
+
+            shootY = shoot[0] - 'A';
+            shootX = shoot[1] - '1';
+            if (shootX == 1) {
+                if (shoot[2] == '0')
+                    shootX = 9;
+            }
+        } while (shootX > 10 || shootX <= -1 || shootY > 10 || shootY <= -1);
+        //printf("%d,%d", shootX, shootY);
+        switch (type_missile) {
+            case 1 :
+                fire_missile(shootX,shootY,&boat_grid,&user_grid,liste_bateaux);
+                break;
+            case 2 :
+                fire_tactical(shootX,shootY,&boat_grid,&user_grid,liste_bateaux);
+                break;
+            //case 3 :
+                fire_bomb(shootX,shootY,&boat_grid,&user_grid,liste_bateaux);
+                break;
+            case 4 :
+                fire_artillery(shootX,shootY,&boat_grid,&user_grid,liste_bateaux);
+                break;
+            default :
+                printf("Erreur : valeur 'type missile' invalide");
+                exit(0);
+        }
+        fire_artillery(shootX,shootY,&boat_grid, &user_grid,liste_bateaux);
+
+    } while(win(liste_bateaux, nb_bateaux) != 0);
 
     return (0);
 }
+
